5ac2366c8676dcba9c888d4d2cfcf1b88dfcdbec
RosaChoi <rosa.uniq@gmail.com>
2015-01-26 16:25:05 -0800

    inital commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,13 @@
+class MoviesController < ApplicationController
+  def all
+    @movies = Movie.all
+    render json: @movies
+  end
+
+  def search
+    @movies = (params["y"] ? Movie.where(year: params["y"]) : Movie.all)
+    @movies = @movies.select { |movie| movie if movie.title.downcase.include?(params["t"].downcase)  } if params["t"]
+    render json: @movies
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Movie.create(title: "The Grand Budapest Hotel", year: 2014, plot: "The adventures of Gustave H, a legendary concierge at a famous hotel from the fictional Republic of Zubrowka between the first and second World Wars, and Zero Moustafa, the lobby boy who becomes his most trusted friend.", image_url: "http://ia.media-imdb.com/images/M/MV5BMzM5NjUxOTEyMl5BMl5BanBnXkFtZTgwNjEyMDM0MDE@._V1_SX640_SY720_.jpg")
+Movie.create(title: "The Fifth Element ", year: 1997, plot: "In the colorful future, a cab driver unwittingly becomes the central figure in the search for a legendary cosmic weapon to keep Evil and Mr Zorg at bay.", image_url: "http://ia.media-imdb.com/images/M/MV5BMTkzOTkwNTI4N15BMl5BanBnXkFtZTYwMDIzNzI5._V1_SX640_SY720_.jpg")
+Movie.create(title: "Fight Club", year: 1999, plot: "An insomniac office worker looking for a way to change his life crosses paths with a devil-may-care soap maker and they form an underground fight club that evolves into something much, much more...", image_url: "http://ia.media-imdb.com/images/M/MV5BMjIwNTYzMzE1M15BMl5BanBnXkFtZTcwOTE5Mzg3OA@@._V1_SX640_SY720_.jpg")
+Movie.create(title: "The Prestige", year: 2006, plot: "Two stage magicians engage in competitive one-upmanship in an attempt to create the ultimate stage illusion.", image_url: "http://ia.media-imdb.com/images/M/MV5BMjA4NDI0MTIxNF5BMl5BanBnXkFtZTYwNTM0MzY2._V1_SX640_SY720_.jpg")
+Movie.create(title: "Rounders", year: 1998, plot: "A young man is a reformed gambler who must return to playing big stakes poker to help a friend pay off loan sharks.", image_url: "http://ia.media-imdb.com/images/M/MV5BMTc4OTcxNDY2Nl5BMl5BanBnXkFtZTgwNDg0MzkxMDE@._V1_SX640_SY720_.jpg")
+Movie.create(title: "Limitless", year: 2011, plot: "With the help of a mysterious pill that enables the user to access 100 percent of his brain abilities, a struggling writer becomes a financial wizard, but it also puts him in a new world with lots of dangers.", image_url: "http://ia.media-imdb.com/images/M/MV5BMTY3NjczNzc5Nl5BMl5BanBnXkFtZTcwMzA2MzQyNA@@._V1_SX640_SY720_.jpg")
+Movie.create(title: "Sherlock Holmes", year: 2009, plot: "Detective Sherlock Holmes and his stalwart partner Watson engage in a battle of wits and brawn with a nemesis whose plot is a threat to all of England.", image_url: "http://ia.media-imdb.com/images/M/MV5BMTg0NjEwNjUxM15BMl5BanBnXkFtZTcwMzk0MjQ5Mg@@._V1_SX640_SY720_.jpg")
+Movie.create(title: "Mystic River", year: 2003, plot: "With a childhood tragedy that overshadowed their lives, three men are reunited by circumstance when one has a family tragedy.", image_url: "http://ia.media-imdb.com/images/M/MV5BMTIzNDUyMjA4MV5BMl5BanBnXkFtZTYwNDc4ODM3._V1_SX640_SY720_.jpg")
+Movie.create(title: "Inception", year: 2010, plot: "A thief who steals corporate secrets through use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.", image_url: "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX640_SY720_.jpg")
+Movie.create(title: "Eternal Sunshine of the Spotless Mind", year: 2004, plot: "When their relationship turns sour, a couple undergoes a procedure to have each other erased from their memories. But it is only through the process of loss that they discover what they had to begin with.", image_url: "http://ia.media-imdb.com/images/M/MV5BMTY4NzcwODg3Nl5BMl5BanBnXkFtZTcwNTEwOTMyMw@@._V1_SX640_SY720_.jpg")
